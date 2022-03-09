8d87c3d881013fb756940cbc6fb78527e5e73320
DESKTOP-3AEO532\User <sout.rahim@gmail.com>
2022-03-09 15:10:18 +0700

    ebook

--- login.php
+++ login.php
@@ -1,48 +1,47 @@
-<?php 
-session_start(); 
-include "db_conn.php";
+<!doctype html>
+<html lang="en">
 
-if (isset($_POST['uname']) && isset($_POST['password'])) {
-    function validate($data){
-       $data = trim($data);
-       $data = stripslashes($data);
-       $data = htmlspecialchars($data);
-       return $data;
-    }
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <meta name="msapplication-tap-highlight" content="no">
+    <link href="main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
 
-    $uname = validate($_POST['uname']);
-    $pass = validate($_POST['password']);
-
-    if (empty($uname)) {
-        header("Location: index.php?error=User Name is required");
-        exit();
-    }else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
-        exit();
-    }else{
-        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-                echo "Logged in!";
-                $_SESSION['user_name'] = $row['user_name'];
-                $_SESSION['name'] = $row['name'];
-                $_SESSION['id'] = $row['id'];
-                header("Location: home.php");
-                exit();
-            }else{
-                header("Location: index.php?error=Incorect User name or password");
-                exit();
-            }
-
-        }else{
-            header("Location: index.php?error=Incorect User name or password");
-            exit();
-        }
-    }
-}else{
-    header("Location: index.php");
-    exit();
-
-}
+        <div class="main-card mb-3 card">
+            <div class="card-body"><h5 class="card-title">Login</h5>
+                <form class="">
+                    <div class="position-relative row form-group"><label for="exampleEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10"><input name="email" id="exampleEmail" placeholder="with a placeholder" type="email" class="form-control"></div>
+                    </div>
+                    <div class="position-relative row form-group"><label for="examplePassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10"><input name="password" id="examplePassword" placeholder="password placeholder" type="password" class="form-control"></div>
+                    </div>
+    
+                    <div class="position-relative row form-check">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button class="btn btn-secondary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+                            
+        <div style=" padding: 20px">
+            <div class="row">
+                <div class="col-md-12" style="text-align:center">
+                        <i>Copyright 2022 SOUTRUOSHIM All right reversed.</i>
+                </div>
+            </div>
+                
+        </div>
+    </div>
+</body>
+</html>              
