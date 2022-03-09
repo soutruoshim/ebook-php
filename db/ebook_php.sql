73ad87e27d2faecf169deedd835aa005b2080b20
DESKTOP-3AEO532\User <sout.rahim@gmail.com>
2022-03-09 16:58:10 +0700

    origin

--- db/ebook_php.sql
+++ db/ebook_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 04, 2022 at 02:03 AM
+-- Generation Time: Mar 09, 2022 at 09:57 AM
 -- Server version: 5.7.36
 -- PHP Version: 8.0.13
 
@@ -42,15 +42,7 @@ CREATE TABLE IF NOT EXISTS `books` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`id`, `author`, `title`, `ISBN`, `category_id`, `publication_year`, `price`, `image`, `book_file`, `detail`, `created_at`, `updated_at`) VALUES
-(1, NULL, 'SDFSDf', '3432432', '1', '2021', 2400, '1644566453.jpg', '1644566453.pdf', 'q342134', '2022-02-11 01:00:53', '2022-02-11 01:00:53'),
-(2, '345', '345', '345', '345', '345', NULL, 'a4e0e4cd775ae985c90fcd597e5ff39d56c9a2b71646299849', 'ae538a94ffaa21de346014d16e172830d80d01f11646299849', '345345', NULL, NULL);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -66,16 +58,15 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
-(24, 'KH3', '2343', NULL, NULL),
-(25, 'KH5', '234H', NULL, NULL),
-(23, 'KH', 'Pro', NULL, NULL);
+(1, 'Programming', 'programming', '2022-03-09 09:55:59', NULL),
+(2, 'Business & Economics', 'business_&_economics', '2022-03-09 09:56:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -87,14 +78,21 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `firstname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `lastname` int(11) DEFAULT NULL,
+  `lastname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'souts', 'ruoshims', 'sout.rahim@gmail.com', '0000', '2022-03-04 02:22:58', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
