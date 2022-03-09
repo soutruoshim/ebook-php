e893a9ab3973dabefcd9836efcabfffb36edca9d
DESKTOP-3AEO532\User <sout.rahim@gmail.com>
2022-03-09 09:15:53 +0700

    ebook

--- admin/queries/delete_ebook.php
+++ admin/queries/delete_ebook.php
@@ -5,13 +5,15 @@
     $id = $_POST['id'];
 
     $a = new database();
-    // $a->select('books',$rows="*",$where = "id='$id'");
-    // while ($row = mysqli_fetch_assoc($result)){
-    //     $image = $row['image'];
-    //     $file = $row['book_file'];
-    //     unlink(WEB_URL.'images/ebook/'.$image);
-    //     unlink(WEB_URL. 'files/ebook/'.$file);
-    // }
+    $a->select("books","*","id='$id'");
+    $result = $a->sql;
+
+    $row = mysqli_fetch_assoc($result);
+    $image = $row['image'];
+    $file = $row['book_file'];
+    unlink(WEB_URL.'images/ebook/'.$image);
+    unlink(WEB_URL. 'files/ebook/'.$file);
+
     $a->delete('books',"id='$id'");
     if ($a == true) {
         header('location:../ebook.php');
